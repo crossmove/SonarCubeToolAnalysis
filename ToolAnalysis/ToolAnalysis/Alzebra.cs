928a7d12c19a2c1072822044db7f5c77e19b6029
Pradeep <Pradeep.exe@gmail.com>
2020-02-02 22:58:32 +0530

    Commi

---
+++ ToolAnalysis/ToolAnalysis/Alzebra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolAnalysis
+{
+    class Alzebra
+    {
+        public void AlzebMethod1()
+        {
+
+        }
+
+        public void AlzebMethod2()
+        {
+
+        }
+    }
+}
---
+++ ToolAnalysis/ToolAnalysis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Add(int a)
+        {
+        }
+        static void Sub(int b)
+        {
+        }
+
+    }
+}
